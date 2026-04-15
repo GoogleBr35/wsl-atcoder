b0d08a9f32aee9d1d1ad896232628ecb985944b9
GoogleBr35 <soyasato15@gmail.com>
2026-04-15 11:34:38 +0900

    deer

--- deer/practice_problems/5_2/Orange.cpp
+++ deer/practice_problems/5_2/Orange.cpp
@@ -6,8 +6,19 @@ int main(void) {
     // inputs
     int a, b, w;
     cin >> a >> b >> w;
+    w *= 1000; // gram変換
+
+    int minimum = 1000001;
+    int maximum = -1;
+    for(int n = 1; n <= 1000000; ++n) {
+        if(a * n <= w && w <= b * n) {
+            minimum = min(minimum, n);
+            maximum = max(maximum, n);
+        }
+    }
 
     // outputs
-
+    if(maximum < 0) cout << "UNSATISFIABLE" << endl;
+    else cout << minimum << " " << maximum << endl;
     return 0;
 }
