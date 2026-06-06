4c1f4d52074518341631022e53f88125e5c26f33
GoogleBr35 <soyasato15@gmail.com>
2026-06-06 22:28:58 +0900

    abc461

---
+++ abc/abc461/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(void) {
+    // inputs
+    int a, d;
+    cin >> a >> d;
+
+    if(a <= d) cout << "Yes" << endl;
+    else cout << "No" << endl;
+   
+    return 0;
+}
---
+++ abc/abc461/d.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(void) {
+    // inputs
+    ll h, w, k;
+    cin >> h >> w >> k;
+    vector<string> board(h);
+    for(int i = 0; i < h; ++i) {
+        cin >> board[i];
+    }
+
+    ll max = 0;
+    ll count = 0;
+    for(int i = 0; i < n; ++i) {
+        if(i * i >= k)
+    }
+    
+    // outputs
+    cout << max << endl;
+
+    return 0;
+}
